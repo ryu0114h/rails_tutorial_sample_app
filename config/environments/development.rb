3213bce2620dce9dd076537d0eca02558bd715c1
ryu0114h <ryu0114h@gmail.com>
2021-04-16 03:07:25 +0900

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  host = 'localhost:3000'                     # ローカル環境
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  Rails.application.routes.default_url_options[:host] = host
 
   config.action_mailer.perform_caching = false
 
